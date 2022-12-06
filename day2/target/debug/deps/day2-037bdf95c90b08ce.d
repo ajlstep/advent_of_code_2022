8c49ff38f9a5aa222ba6fd8d667082f8f65dca8f
bl.ajl <bl.ajlhimik@gmail.com>
2022-12-06 14:07:06 +0200

    remove target/day2

--- day2/target/debug/deps/day2-037bdf95c90b08ce.d
+++
@@ -1,5 +0,0 @@
-d:\lang\rust\advent_of_code_2022\day2\target\debug\deps\day2-037bdf95c90b08ce.rmeta: src\main.rs
-
-d:\lang\rust\advent_of_code_2022\day2\target\debug\deps\day2-037bdf95c90b08ce.d: src\main.rs
-
-src\main.rs:
--- day2/target/debug/deps/day2-2e3f06a4c0ba9993.d
+++
@@ -1,5 +0,0 @@
-d:\lang\rust\advent_of_code_2022\day2\target\debug\deps\day2-2e3f06a4c0ba9993.rmeta: src\main.rs
-
-d:\lang\rust\advent_of_code_2022\day2\target\debug\deps\day2-2e3f06a4c0ba9993.d: src\main.rs
-
-src\main.rs:
